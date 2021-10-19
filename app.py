ced713c6946dafd0c2eb733b30acfa8c301586b1
Andrés Cendales <andres01660@gmail.com>
2021-10-19 17:38:09 -0500

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# Application Entrypoint
+
+# Flask
+from flask import Flask
+from flask_restful import Api
+
+# Utils
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
