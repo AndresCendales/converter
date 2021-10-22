c64ab8e1e010ea08f1919234f4f816df5db51f78
Andrés Cendales <andres01660@gmail.com>
2021-10-22 18:46:08 -0500

    feat: add update task

--- celery/tasks.py
+++ celery/tasks.py
@@ -19,34 +19,40 @@ celery = Celery(
 
 
 @celery.task(name='tasks.convert')
-def convert(original_filename, new_format):
+def convert(user_id, original_filename, new_format, filename_to_delete=""):
     """
     convert orchestra the conversion of file to new format
+    :param user_id: user_id of user which request the conversion
     :param original_filename: original string filename with original format
     :param new_format: new format to convert the file
+    :param filename_to_delete: filename which will be deleted if exists
     :return:
     """
-    logger.info('CeleryTasks', 'convert', 'Iniciando conversion...')
-
     new_filename = original_filename.rsplit('.', 1)[0] + "." + new_format
-    execute_conversion(original_filename, new_filename)
-    update_database_status(original_filename, new_filename)
+
+    execute_conversion(user_id, original_filename, new_filename)
+    update_database_status(user_id, original_filename, new_filename)
+
+    if filename_to_delete != "":
+        delete(user_id, filename_to_delete)
+
     if os.getenv('APP_MODE') != "TEST":
         send_email()
 
 
-def execute_conversion(original_filename, new_filename):
+def execute_conversion(user_id, original_filename, new_filename):
     """
     Execute_conversion pass command line to the system wich execute the file conversion
+    :param user_id: user_id to save the files in that folder
     :param original_filename
     :param new_filename:
     :return:
     """
-    os.system(f"ffmpeg -i files/{original_filename} files/{new_filename} -y")
+    os.system(f"ffmpeg -i files/{user_id}/{original_filename} files/{user_id}/{new_filename} -y")
     logger.info('CeleryTasks', 'execute_conversion', 'Conversion finalizada')
 
 
-def update_database_status(original_filename, new_filename):
+def update_database_status(user_id, original_filename, new_filename):
     """
     update_database_status change the status to processed for the given filename
     :param original_filename
@@ -61,7 +67,7 @@ def update_database_status(original_filename, new_filename):
         cursor = connection.cursor()
 
         # Executing a SQL query
-        query = f"UPDATE tasks SET status = 'processed', new_file_path = '{new_filename}' WHERE original_file_path = '{original_filename}';"
+        query = f"UPDATE tasks SET status = 'processed', new_file_path = '{new_filename}' WHERE original_file_path = '{original_filename}' AND user_id = {user_id};"
         cursor.execute(query)
         connection.commit()
 
@@ -74,8 +80,14 @@ def update_database_status(original_filename, new_filename):
             logger.info('CeleryTasks', 'error', f'No se puede actualizar el estatus: {error}')
 
 
+def delete(user_name, filename_to_delete):
+    """
+    delete a file
+    """
+    os.system(f"rm files/{user_name}/{filename_to_delete}")
+
+
 def send_email():
     logger.info('CeleryTasks', 'send_email', 'Email enviado correctamente')
     # ToDo
     pass
-
