8a0016ce34e62d1a872027d3f99bfc059d6b87af
Andrés Cendales <andres01660@gmail.com>
2021-10-23 18:42:12 -0500

    feat: fix values on get endpoint

--- api/tasks/views.py
+++ api/tasks/views.py
@@ -53,22 +53,24 @@ class TasksView(MethodView):
             return {"message": "Filename invalido"}, 400
         file.filename = secure_filename(file.filename)
         if not allowed_file(file.filename):
-            return {"message": "Archivo no permitido. Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
+            return {
+                       "message": "Archivo no permitido. Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
 
         task = Task.query.filter_by(original_file_path=file.filename, user_id=user.id).first()
         if task is not None:
-            return {"message": "Ya existe un archivo con este nombre. Para actualizar el formato se debe usar el endpoint via PUT",
-                    "id_task": task.id }, 400
+            return {
+                       "message": "Ya existe un archivo con este nombre. Para actualizar el formato se debe usar el endpoint via PUT",
+                       "id_task": task.id}, 400
 
         new_format = request.form.get('newFormat')
         if new_format is None:
             return {"message": "Por favor incluye el formato al que deseas convertir"}, 400
         if new_format not in ALLOWED_EXTENSIONS:
-            return {"message": "newFormat invalido, Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
+            return {
+                       "message": "newFormat invalido, Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
 
         filepath = os.path.join(os.getenv("UPLOAD_FOLDER"), f"{user.id}/" + file.filename)
         file.save(filepath)
-
 
         new_task = Task(
             user_id=user.id,
@@ -106,21 +108,23 @@ class TasksView(MethodView):
         try:
             user = User.query.filter_by(username=get_jwt_identity()).first()
             max = request.args.get('max') or None
-            if (max == '0'):
+            if max == '0':
                 max = None
-            order = request.args.get('order') or 0
+
+            order = request.args.get('order', '0') or 0
             query_order = Task.id.asc() if order == '0' else Task.id.desc()
+
             tasks = Task.query.filter_by(user_id=user.id).order_by(query_order).limit(max).all()
-            if (len(tasks) == 0):
+            if len(tasks) == 0:
                 logger.info('TasksView', 'get', 'Consulta de tareas sin resultado')
-                return 'El usuario no tiene tareas', 400
+                return {"mensaje": 'El usuario no tiene tareas'}, 400
             serialized_tasks = [task_schema.dump(task) for task in tasks]
             logger.info('TasksView', 'get', 'Consulta de tareas exitosa')
             return {'tasks': serialized_tasks}, 200
         except Exception as err:
             print(err)
             logger.error('TasksView', 'get', 'error al consultar las tareas: ' + err)
-            return {'msg': 'server error'}, 500
+            return {'mensaje': 'server error'}, 500
 
 
 class TaskView(MethodView):
@@ -140,16 +144,16 @@ class TaskView(MethodView):
             task = Task.query.get(id_task)
             if task is None:
                 logger.info('TaskView', 'get', 'Tarea no encontrada')
-                return {'task': 'not found'}, 404
+                return {'mensaje': 'Tarea no encontrada'}, 404
             if task.user_id != user.id:
                 logger.info('TaskView', 'get', 'Tarea de otro usuario')
-                return {'task': 'the task is of the other user'}, 404
+                return {'mensaje': 'Tarea de otro usuario'}, 401
 
             logger.info('TaskView', 'get', 'Tarea encontrada')
             return task_schema.dump(task), 200
         except:
             logger.error('TaskView', 'get', 'error al consultar las tareas')
-            return {'msg': 'server error'}, 500
+            return {'mensaje': 'server error'}, 500
 
     @jwt_required()
     def put(self, id_task):
@@ -165,7 +169,8 @@ class TaskView(MethodView):
             return {"mensaje": "Debe enviar el nuevo formato al cual desea convertir el archivo."}, 400
 
         if new_format not in ALLOWED_EXTENSIONS:
-            return {"message": "newFormat invalido, Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
+            return {
+                       "message": "newFormat invalido, Las extensiones soportadas son {'mp3', 'acc', 'ogg', 'wav', 'wma'} "}, 400
 
         task = Task.query.filter_by(id=id_task).first()
         if task is None:
