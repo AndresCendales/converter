5e5bf8b97cdf6af138685769d810a9b222a6fea7
Andrés Cendales <andres01660@gmail.com>
2021-10-23 17:45:26 -0500

    feat: add email notification

--- celery/tasks.py
+++ celery/tasks.py
@@ -6,7 +6,7 @@ import psycopg2
 from psycopg2 import Error
 
 # Email
-from notificator.notifier import Notifier
+import smtplib
 
 # Utils
 import os
@@ -45,7 +45,7 @@ def convert(user_id, original_filename, new_format, filename_to_delete=""):
             new_format=new_format,
             new_file_path=new_filename
         )
-        logger.info('CeleryTasks', 'send_email', 'Email enviado correctamente')
+
 
 
 def execute_conversion(user_id, original_filename, new_filename):
@@ -94,3 +94,42 @@ def delete(user_name, filename_to_delete):
     delete a file
     """
     os.system(f"rm files/{user_name}/{filename_to_delete}")
+
+
+sender = "Coverter <notifier@converter.com>"
+receivers = ['Anderson Santamaria <andersonsantamaria3@gmail.com>']
+
+
+class Notifier:
+    def send_notification_to_download(self, original_file_path, new_format, new_file_path):
+        self.get_email(original_file_path, new_format, new_file_path)
+        self.send_notification(self.email, os.getenv("MAILGUN_PASSWORD"))
+        return "email sent.", 200
+
+    def get_email(self, original_file_path, new_format, new_file_path):
+        subject = "Converted document!"
+
+        own_message = """Hi! <br/> <br/> 
+        The document that you uploaded in the route <b>%s</b>, to be converted to one of a kind <b>%s</b> 
+        it's ready, you can find it with on the route <b>%s</b>
+        <br/> <br/>
+        Best regards from Converter.
+        """ % (original_file_path, new_format, new_file_path)
+
+        self.email = """From: %s 
+        To: %s 
+        MIME-Version: 1.0 
+        Content-type: text/html 
+        Subject: %s 
+        %s
+        """ % (sender, receivers, subject, own_message)
+
+    def send_notification(self, email, smtp_password):
+        try:
+            s = smtplib.SMTP(host='smtp.mailgun.org', port=587)
+            s.starttls()
+            s.login("postmaster@sandbox5a390ccc8a52448fa5a7f8c6d603e025.mailgun.org", smtp_password)
+            s.sendmail(sender, receivers, email)
+            logger.info('CeleryTasks', 'send_email', 'Email enviado correctamente')
+        except Exception as e:
+            logger.error("EmailFailed", 'email_not_sended', f"error: {e}")
