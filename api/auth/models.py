b7fc986c5084f01b85884cd39b0e7e4b9d13b6c0
Andrés Cendales <andres01660@gmail.com>
2021-10-23 20:04:46 -0500

    fix: fix length email

--- api/auth/models.py
+++ api/auth/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
 
 
 class UserSchema(SQLAlchemyAutoSchema):
