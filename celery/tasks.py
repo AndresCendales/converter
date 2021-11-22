89c55e50ddc6d94a88c5fbf26f56bfb74a68bc20
asantaba <as.santamaria@uniandes.edu.co>
2021-11-21 23:32:17 -0500

    see delta

--- celery/tasks.py
+++ celery/tasks.py
@@ -38,6 +38,8 @@ def convert(user_id, original_filename, new_format, created_at, filename_to_dele
     """
     created_at = datetime.fromisoformat(created_at)
     difference = datetime.now() - created_at
+    logger.info("ProccesViewer", 'see delta',
+                    f"delta: {difference.total_seconds()} s")
     if difference.total_seconds() > int(os.getenv("LIMIT_PROCESSING_TEST", "600")):
         logger.info("ProccesOutOfTime", 'test out of time',
                     f"El proceso de conversion tardo: {difference.total_seconds()} s")
