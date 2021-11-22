a98a4122021e7369e4c968ba1697cffd1fd0cf53
asantaba <as.santamaria@uniandes.edu.co>
2021-11-21 22:23:48 -0500

    add file download

--- api/files/views.py
+++ api/files/views.py
@@ -10,6 +10,8 @@ from api.tasks.models import Task
 # Utils
 import os
 
+from util import s3_service
+
 
 class FilesView(MethodView):
     @jwt_required()
@@ -25,6 +27,8 @@ class FilesView(MethodView):
         if task is None:
             return {"mensaje": "Aun no esta listo el archivo"}
 
-        path = os.path.join(os.getenv("UPLOAD_FOLDER"), f"{user.id}/")
-
+        #path = os.path.join(os.getenv("UPLOAD_FOLDER"), f"{user.id}/")
+        os.system(f"mkdir -p files/{user.id}")
+        s3_path = "files/"+str(user.id)+"/"+task.original_file_path
+        path = s3_service.s3_download_file(s3_path, str(user.id)+"/"+filename)
         return send_from_directory(path, filename)
